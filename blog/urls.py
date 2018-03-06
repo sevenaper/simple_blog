71ff15e501575db98e9840740112b53b95fbe178
aper <lxsdwlhh@163.com>
2018-03-06 16:54:02 +0800

    again

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 # start with blog
 urlpatterns = [
-    # http://127.0.0.1:8000/blog/1
+    # http://127.0.0.1:8000/blog/
+    path('', views.blog_list, name='blog_list'),
     path('<int:blog_pk>', views.blog_detail, name="blog_detail"),
     path('type/<int:blog_type_pk>', views.blogs_with_type, name="blogs_with_type")
 
