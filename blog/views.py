f3f7228ab858ed9aa481a230040a35fe4c0de528
aper <lxsdwlhh@163.com>
2018-03-07 19:28:45 +0800

    bootstrap 2.0

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .models import Blog, BlogType
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
-    context['blog_types']=BlogType.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     context['blogs_count'] = Blog.objects.all().count()
     return render_to_response('blog_list.html', context)
 
@@ -20,6 +20,7 @@ def blog_detail(request, blog_pk):
 def blogs_with_type(request, blog_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blogs_with_type.html', context)
