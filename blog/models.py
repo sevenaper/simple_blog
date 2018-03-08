2e375de69eae0836577de0b56e1c600b31d79597
aper <lxsdwlhh@163.com>
2018-03-08 23:19:31 +0800

    分页功能

--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,5 @@ class Blog(models.Model):
 
     def __str__(self):
         return "<Blog:%s> " % self.title
+    class Meta:
+        ordering=['-create_time']
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Blog, BlogType
 
 
 # Create your views here.
 def blog_list(request):
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list, 10)  # 十个博客进行分页
+    page_num = request.GET.get('page', 1)  # get请求获取页码
+    page_of_blogs = paginator.get_page(page_num)
+
     context = {}
-    context['blogs'] = Blog.objects.all()
+    context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     context['blogs_count'] = Blog.objects.all().count()
     return render_to_response('blog_list.html', context)
