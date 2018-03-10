4f72686217a45adf6ae482bde9bda32cc3f024f6
aper <lxsdwlhh@163.com>
2018-03-10 16:53:13 +0800

    优化分页

--- blog/views.py
+++ blog/views.py
@@ -7,13 +7,37 @@ from .models import Blog, BlogType
 def blog_list(request):
     blogs_all_list = Blog.objects.all()
     paginator = Paginator(blogs_all_list, 10)  # 十个博客进行分页
-    page_num = request.GET.get('page', 1)  # get请求获取页码
+    page_num = request.GET.get('page', 10)  # get请求获取页码
     page_of_blogs = paginator.get_page(page_num)
+    current_page = page_of_blogs.number
+    if current_page == 1 or current_page == 2:
+        if paginator.num_pages <= 5:
+            page_range = list(range(1, paginator.num_pages))
+        else:
+            page_range = list([1, 2, 3, 4, 5])
+    elif current_page == paginator.num_pages or current_page == paginator.num_pages - 1:
+        if paginator.num_pages <= 5:
+            page_range = list(range(1, paginator.num_pages))
+        else:
+            page_range = list([paginator.num_pages - 4, paginator.num_pages - 3, paginator.num_pages - 2,
+                               paginator.num_pages - 1, paginator.num_pages])
+    else:
+        page_range = list(range(max(current_page - 1, 1), current_page)) + \
+                     list(range(current_page, min(current_page + 1, paginator.num_pages) + 1))
+    if page_range and page_range[0] >= 3:
+        page_range.insert(0, '...')
+    if page_range and paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    if page_range and page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range and page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
 
     context = {}
+    context['blogs'] = page_of_blogs.object_list
     context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
-    context['blogs_count'] = Blog.objects.all().count()
+    context['page_range'] = page_range
     return render_to_response('blog_list.html', context)
 
 
@@ -26,7 +50,40 @@ def blog_detail(request, blog_pk):
 def blogs_with_type(request, blog_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
+    blogs_all_list = Blog.objects.filter(blog_type=blog_type)
+    paginator = Paginator(blogs_all_list, 10)  # 十个博客进行分页
+    page_num = request.GET.get('page', 10)  # get请求获取页码
+    page_of_blogs = paginator.get_page(page_num)
+    current_page = page_of_blogs.number
+    if current_page == 1 or current_page == 2:
+        if paginator.num_pages <= 5:
+            page_range = list(range(1, paginator.num_pages))
+        else:
+            page_range = list([1, 2, 3, 4, 5])
+    elif current_page == paginator.num_pages or current_page == paginator.num_pages - 1:
+        if paginator.num_pages <= 5:
+            page_range = list(range(1, paginator.num_pages))
+        else:
+            page_range = list([paginator.num_pages - 4, paginator.num_pages - 3, paginator.num_pages - 2,
+                               paginator.num_pages - 1, paginator.num_pages])
+    else:
+        page_range = list(range(max(current_page - 1, 1), current_page)) + \
+                     list(range(current_page, min(current_page + 1, paginator.num_pages) + 1))
+    if page_range and page_range[0] >= 3:
+        page_range.insert(0, '...')
+    if page_range and paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    if page_range and page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range and page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
+    context = {}
     context['blog_type'] = blog_type
-    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blogs'] = page_of_blogs.object_list
+    context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
+    context['page_range'] = page_range
+    context['blogs_all_list'] = blogs_all_list
+
     return render_to_response('blogs_with_type.html', context)
