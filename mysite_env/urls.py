47106338087194c37767c9928c8bea03678b5e92
aper <lxsdwlhh@163.com>
2018-03-11 18:25:27 +0800

    图片上传问题仍要解决

--- mysite_env/urls.py
+++ mysite_env/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import blog_list
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('blog/', include('blog.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
