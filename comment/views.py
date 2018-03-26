4443172b31f6b34571e8a030d9960af2c2cad626
aper <lxsdwlhh@163.com>
2018-03-26 13:31:26 +0800

    login

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Comment
+
 
 # Create your views here.
+def update_comment(request):
+    pass
--- mysite_env/views.py
+++ mysite_env/views.py
@@ -5,6 +5,7 @@ from django.contrib import auth
 from django.utils import timezone
 from django.db.models import Sum
 from django.core.cache import cache
+from django.urls import reverse
 from read_statistics.utils import get_seven_days_date, get_today_hot_data, get_yesterday_hot_data
 from blog.models import Blog
 
@@ -42,8 +43,9 @@ def login(request):
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
     user = auth.authenticate(request, username=username, password=password)
+    referer = request.META.get('HTTP_REFERER', reverse('home'))
     if user is not None:
         auth.login(request, user)
-        return redirect('/')
+        return redirect(referer)
     else:
         return render(request, 'error.html', {'message': '用户名或密码不正确'})
